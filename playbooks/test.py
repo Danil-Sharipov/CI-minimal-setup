e1b8303a66d6c913b580b504b8b3f2f95cd120ad
Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
2024-01-13 02:53:01 +0300

    delete

--- playbooks/test.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-try:
-    # пытаемся подключиться к базе данных
-    conn = psycopg2.connect(dbname='mlflow', user='mlflow', password='mlflow', host='192.168.56.12')
-except:
-    # в случае сбоя подключения будет выведено сообщение в STDOUT
-    print('Can`t establish connection to database')
