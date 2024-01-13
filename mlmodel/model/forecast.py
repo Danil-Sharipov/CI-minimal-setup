6b373900988bd7f2ba97902e225ef6dfd09466dd
Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
2024-01-14 00:15:26 +0300

    forecast.py_update

--- mlmodel/model/forecast.py
+++ mlmodel/model/forecast.py
@@ -3,9 +3,16 @@ from os.path import exists
 from os import mkdir
 from gluonts.dataset.pandas import PandasDataset
 from gluonts.mx.model.deepar import DeepAREstimator
+from gluonts.trainer import Trainer
 import numpy as np
 from pathlib import Path
-#https://www.kaggle.com/code/snnclsr/gluonts-deepar-model-with-validation
+import optuna
+import torch
+import mlflow
+from gluonts.evaluation import make_evaluation_predictions
+from gluonts.evaluation import Evaluator
+from gluonts.dataset.field_names import FieldName
+
 def main(json):
     df = pd.DataFrame.from_dict(json,orient='index')
     if exists('model'):
@@ -20,45 +27,54 @@ def create(df):
     train_ds = PandasDataset.from_long_dataframe(train, target='target', item_id='ident',
                                                  timestamp='end', freq='D')
     # https://ts.gluon.ai/stable/api/gluonts/gluonts.torch.model.deepar.estimator.html
-    estimator = DeepAREstimator(freq='D', prediction_length=len(valid), num_layers=3)
-    # число эпох надо настроить
+    estimator = DeepAREstimator(freq='D', prediction_length=30, trainer = Trainer(epochs=20))
     predictor = estimator.train(train_ds, num_workers=2)
-    pred = list(predictor.predict(train_ds))
-    all_preds = list()
 
-    def wmape(y_true, y_pred):
-        return np.abs(y_true - y_pred).sum() / np.abs(y_true).sum()
+    valid_ds = PandasDataset.from_long_dataframe(valid, target='target', item_id='ident',
+                                                 timestamp='end', freq='D')
 
-    for item in pred:
-        family = item.item_id
-        p = item.samples.mean(axis=0)
-        p10 = np.percentile(item.samples, 10, axis=0)
-        p90 = np.percentile(item.samples, 90, axis=0)
-        dates = pd.date_range(start=item.start_date.to_timestamp(), periods=len(p), freq='D')
-        family_pred = pd.DataFrame({'end': dates, 'ident': family, 'pred': p, 'p10': p10, 'p90': p90})
-        all_preds += [family_pred]
-    all_preds = pd.concat(all_preds, ignore_index=True)
-    all_preds = all_preds.merge(valid, on=['end', 'ident'], how='left')
+    ###############################
+                # metrics
+    ###############################
+    forecast_it, ts_it = make_evaluation_predictions(
+        dataset=valid_ds,
+        predictor=predictor,
+    )
+    forecasts = list(forecast_it)
+    tss = list(ts_it)
+    evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
+    agg_metrics, item_metrics = evaluator(tss, forecasts)
+    # print(json.dumps(agg_metrics, indent=4))
+    ###############################
+              # mlflow
+    ###############################
+
+
+
+
+
+
     mkdir('model')
-    score = wmape(all_preds['target'], all_preds['pred'])
     predictor.serialize(Path("./model/"))
 
 def add(df):
+    predictor = get_predicator()
     pass
 
 def predict(df):
-    from gluonts.evaluation import make_evaluation_predictions
-    df = PandasDataset.from_long_dataframe(pd.DataFrame.from_dict(df,orient='index'), target='target', item_id='ident',
-                                                      timestamp='end', freq='D')
-    forecast_it, ts_it = make_evaluation_predictions(
-        dataset=df,  # test dataset
-        predictor=get_predicator(),  # predictor
-        num_samples=500,  # number of sample paths we want for evaluation
-    )
+    # from know to future 30 day
+    predictor = get_predicator()
+    pass
+    ###############################
+    # predict predicator.predict?
+    ###############################
 
-    return list(ts_it)[0]
+
+
+
 
 def get_predicator():
     from gluonts.model.predictor import Predictor
     predictor_deserialized = Predictor.deserialize(Path("./model/"))
     return predictor_deserialized
+
