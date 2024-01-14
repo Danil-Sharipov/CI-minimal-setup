f65d56198a5c9a7d3c7667590ee5f9f2e4f8c12e
Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
2024-01-15 02:03:25 +0300

    Squashed commit of the following:
    commit efd396946ff98198dedd0ec62b150756814f1615
    Author: Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
    Date:   Mon Jan 15 02:02:12 2024 +0300
    
        dag update
    
    commit 96ac11166a730a49da2f76a4d663afde8de5bb3c
    Author: Danil-Sharipov <121438053+Danil-Sharipov@users.noreply.github.com>
    Date:   Mon Jan 15 02:00:07 2024 +0300
    
        mlflow-add

--- mlmodel/model/forecast.py
+++ mlmodel/model/forecast.py
@@ -21,41 +21,41 @@ def main(json):
         create(df)
 
 def create(df):
-    train = df.loc[df['end'] < '2023-01-01']
-    valid = df.loc[df['end'] >= '2023-01-01']
+    remote_server_uri = "http://mlflow.com"
+    mlflow.set_tracking_uri(uri=remote_server_uri)
+    with mlflow.start_run():
+        mlflow.gluon.autolog()
+        train = df.loc[df['end'] < '2023-01-01']
+        valid = df.loc[df['end'] >= '2023-01-01']
+        prediction_length = 30
+        mlflow.log_param("prediction_length", prediction_length)
 
-    train_ds = PandasDataset.from_long_dataframe(train, target='target', item_id='ident',
-                                                 timestamp='end', freq='D')
-    # https://ts.gluon.ai/stable/api/gluonts/gluonts.torch.model.deepar.estimator.html
-    estimator = DeepAREstimator(freq='D', prediction_length=30, trainer = Trainer(epochs=20))
-    predictor = estimator.train(train_ds, num_workers=2)
+        train_ds = PandasDataset.from_long_dataframe(train, target='target', item_id='ident',
+                                                     timestamp='end', freq='D')
+        # https://ts.gluon.ai/stable/api/gluonts/gluonts.torch.model.deepar.estimator.html
+        estimator = DeepAREstimator(freq='D', prediction_length=prediction_length, trainer = Trainer(epochs=20))
+        predictor = estimator.train(train_ds, num_workers=2)
 
-    valid_ds = PandasDataset.from_long_dataframe(valid, target='target', item_id='ident',
-                                                 timestamp='end', freq='D')
+        valid_ds = PandasDataset.from_long_dataframe(valid, target='target', item_id='ident',
+                                                     timestamp='end', freq='D')
 
-    ###############################
-                # metrics
-    ###############################
-    forecast_it, ts_it = make_evaluation_predictions(
-        dataset=valid_ds,
-        predictor=predictor,
-    )
-    forecasts = list(forecast_it)
-    tss = list(ts_it)
-    evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
-    agg_metrics, item_metrics = evaluator(tss, forecasts)
-    # print(json.dumps(agg_metrics, indent=4))
-    ###############################
-              # mlflow
-    ###############################
-
-
-
-
-
-
-    mkdir('model')
-    predictor.serialize(Path("./model/"))
+        ###############################
+                    # metrics
+        ###############################
+        # forecast_it, ts_it = make_evaluation_predictions(
+        #     dataset=valid_ds,
+        #     predictor=predictor,
+        # )
+        # forecasts = list(forecast_it)
+        # tss = list(ts_it)
+        # evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
+        # agg_metrics, item_metrics = evaluator(tss, forecasts)
+        # # print(json.dumps(agg_metrics, indent=4))
+        # mlflow.log_metric("foo", random() + 2)
+        #
+        mkdir('model')
+        predictor.serialize(Path("./model/"))
+        mlflow.log_artifacts("model")
 
 def add(df):
     predictor = get_predicator()
